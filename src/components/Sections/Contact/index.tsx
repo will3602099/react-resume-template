4c8b78f3871f2d4cfc58bda7b0c308de822d59b6
wulingren <hindenburg.li@temu.com>
2024-06-04 20:10:42 +0800

    保存

--- src/components/Sections/Contact/index.tsx
+++ src/components/Sections/Contact/index.tsx
@@ -1,9 +1,17 @@
-import { DevicePhoneMobileIcon, EnvelopeIcon, MapPinIcon } from '@heroicons/react/24/outline';
+import {
+  DevicePhoneMobileIcon, EnvelopeIcon, MapPinIcon
+} from '@heroicons/react/24/outline';
 import classNames from 'classnames';
-import { FC, memo } from 'react';
+import {
+  FC, memo
+} from 'react';
 
-import { contact, SectionId } from '../../../data/data';
-import { ContactType, ContactValue } from '../../../data/dataDef';
+import {
+  contact, SectionId
+} from '../../../data/data';
+import {
+  ContactType, ContactValue
+} from '../../../data/dataDef';
 import FacebookIcon from '../../Icon/FacebookIcon';
 import GithubIcon from '../../Icon/GithubIcon';
 import InstagramIcon from '../../Icon/InstagramIcon';
