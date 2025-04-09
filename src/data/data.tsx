40da58fe3ab7a6baf5b18166c0be379d7f947c5e
hindenburg.li <hindenburg.li@temu.com>
2025-04-09 20:25:25 +0800

    fix:中文

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -11,10 +11,10 @@ import porfolioImage2 from '../images/portfolio/22222.png';
 import porfolioImage3 from '../images/portfolio/33333.jpg';
 import porfolioImage4 from '../images/portfolio/44444.jpg';
 import porfolioImage8 from '../images/portfolio/88888.jpg';
-import porfolioImage5 from '../images/portfolio/Temu活动.jpg';
-import porfolioImage6 from '../images/portfolio/拼多多主站.jpg';
-import porfolioImage7 from '../images/portfolio/携程.jpg';
-import porfolioImage1 from '../images/portfolio/短视频.jpg';
+import porfolioImage7 from '../images/portfolio/ctrip.jpg';
+import porfolioImage6 from '../images/portfolio/pdd-activity.jpg';
+import porfolioImage1 from '../images/portfolio/short-video.jpg';
+import porfolioImage5 from '../images/portfolio/temu-activity.jpg';
 
 import testimonialImage from '../images/testimonial.webp';
 import profilepic from '../images/willprofilepic.jpg';
