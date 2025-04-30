bfe8e96a57376b7d651f916d5771bc8d3c007192
will <129774157+will3602099@users.noreply.github.com>
2025-04-30 13:33:41 +0800

    Update PasswordGate.tsx
    Signed-off-by: will <129774157+will3602099@users.noreply.github.com>

--- src/components/PasswordGate.tsx
+++ src/components/PasswordGate.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react';
 
-const PASSWORD = process.env.NEXT_PUBLIC_SITE_PASSWORD || "will2025";
+//const PASSWORD = process.env.NEXT_PUBLIC_SITE_PASSWORD || "will2025";
 
 export default function PasswordGate({ children }: { children: React.ReactNode }) {
   return <>{children}</>;
